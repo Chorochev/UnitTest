8bd3d42061d0c30951b5f2986e9ddafdfbe2bfe9
choro <choro@DESKTOP-O2TD37A>
2017-09-02 22:42:59 +0300

    Пример тестирования. Пример использования модульного тестирования кода.

---
+++ UnitTest/TestLibrary/MyMath.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLibrary
+{
+    public class MyMath
+    {
+        /// <summary>
+        /// Сложение двух чмсел
+        /// </summary>
+        /// <param name="number1">первое число</param>
+        /// <param name="number2">второе число</param>
+        /// <returns>сумма двух чисел</returns>
+        public int AdditionTwoNumbers(int number1, int number2)
+        {
+            int result = 0;
+            result = number1 + number2;
+            return result;
+        }
+
+        /// <summary>
+        /// Длинна строки
+        /// </summary>
+        /// <param name="str">строка</param>
+        /// <returns>длинна строки</returns>
+        public int GetLengthString(string str)
+        {
+            if (str == null) return 0;
+            int result = 0;
+            result = str.Length;
+            return result;
+        }
+
+
+    }
+}
---
+++ UnitTest/UnitTestProjectMyMath/MyUnitTestClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestLibrary;
+
+namespace UnitTestProjectMyMath
+{
+    [TestClass]
+    public class MyUnitTestClass
+    {
+        [TestMethod]
+        public void Test_AdditionTwoNumbers()
+        {
+            MyMath myMath = new MyMath();
+            int result = myMath.AdditionTwoNumbers(2, 2);
+            if (result != 4)
+                throw new ArgumentOutOfRangeException("result", result, "Неправильная работа метода AdditionTwoNumbers");
+        }
+        [TestMethod]
+        public void Test_GetLengthString()
+        {
+            MyMath myMath = new MyMath();
+            string testString = "test";
+            int size = myMath.GetLengthString(testString);
+            if (size != 4)
+                throw new ArgumentOutOfRangeException("testString", testString, "Неправильная работа метода GetLengthString");
+        }
+        [TestMethod]
+        public void Test_GetLengthStringNull()
+        {
+            MyMath myMath = new MyMath();
+            string testString = null;
+
+            int size = myMath.GetLengthString(testString);
+            if (size != 0)
+                throw new ArgumentOutOfRangeException("testString", testString, "Неправильная работа метода GetLengthString");
+        }
+
+    }
+}
